a8f7fec01608c023f88e755ef9c1288d353142e3
TopToyTaDa <top0814765001@gmail.com>
2025-02-05 10:35:00 +0700

    แก้ไข1
    แก้ไข1

--- contact.php
+++ contact.php
@@ -16,7 +16,7 @@
 				<li><a href="home.php">หน้าแรก</li>
 				<li><a href="room.php">ห้องพัก&ราคา</li>
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
-				<li><a href="contact.php">ติดต่อ8888</li>
+				<li><a href="contact.php">ติดต่อ88888888</li>
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
